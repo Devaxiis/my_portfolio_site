c20aafb499f86e1b49556ad03e169b0b34eaba83
devaxis2303 <promer2303@gmail.com>
2024-08-29 00:10:10 +0500

    message one

---
+++ lib/changes/links.dart
@@ -0,0 +1,16 @@
+// CV
+const String resume =
+    'https://drive.google.com/file/d/1rMhqXJtO_ryIr4LRKIlHibgeH4itDRKp/view?usp=sharing';
+
+// Github Link
+const String gitHub = 'https://github.com/devaxis2303';
+
+const String youtube =
+    'https://youtube.com/@mirjalolabdunazarov?si=0vF_y1gNMMgO-OE6';
+// Github Link
+const String linkedin = 'https://www.linkedin.com/in/mirjalol-abdunazarov-322b1b286/';
+
+const String telegram = '@abdunazarovvs';
+
+const String upwork = 'https://www.upwork.com/freelancers/~0144b9840a884cb546';
+
---
+++ lib/changes/strings.dart
@@ -0,0 +1,28 @@
+String hellotag = '''Hi there, Welcome to My Space  ''';
+
+String yourname = "I'm Mirjalol Abdunazarov,";
+// String hometitle2 = "A Mobile Application\nDeveloper";
+
+String animationtxt1 = " Mobile Application Developer";
+// String animationtxt2 = " UI/UX Designer";
+// String animationtxt3 = " Web Developer";
+
+//
+String contactHeadding = "Let’s try my service now!";
+String contactSubHeadding =
+    "Let’s work together and make everything super cute and super useful.";
+//
+String miniDescription =
+    "Freelancer providing services for programming. Join me down below and let's get started!";
+
+String servicesSubHeading =
+    "Since the beginning of my journey as a freelance from developer, I've worked in startups company and collaborated with talented people to create digital products for both business and consumer use. I offer a wide range of services programming and teaching.";
+
+
+String protfolioSubHeading =
+    "Since the beginning of my journey as developer, I have created digital products for business and consumer use. This is a little bit.";
+const String aboutMeHeadline =
+    'I\'m Mirjalol Abdunazarov, a Mobile App developer.';
+
+const String aboutMeDetail =
+    'I\'m a Fresh Graduate as Flutter developer from Pdp Academy, UZ. I have been developing mobile apps for over 1 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Been a CEO/Co-Founder of Onyxsio, Sri Lanka. Currently working as an Associate Software Engineer at Onyxsio.';
